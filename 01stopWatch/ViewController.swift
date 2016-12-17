23e265c9f6e0f28130512944160d112578b0bd68
sven <sven.song@qq.com>
2016-12-17 21:20:01 +0800

    Swift-stopWatch

--- 01stopWatch/ViewController.swift
+++ 01stopWatch/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var pausBtn: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var Timer = NSTimer() // 定义定时器变量
+    var counter = 0.0
+    var isRuningTimer = false
+    
+    @IBAction func playBtnAction(sender: AnyObject) {
+        if isRuningTimer {
+            return
+        }
+        // 开启定时器
+        Timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector:#selector(updateCounter), userInfo: nil, repeats: true);
+        Timer.fire()
+        isRuningTimer = true
+    }
+    
+    @IBAction func pausBtnAction(sender: AnyObject) {
+        Timer.invalidate()
+        isRuningTimer = false
+    }
+    
+    @IBAction func resetBtnAction(sender: AnyObject) {
+        Timer.invalidate()
+        isRuningTimer = false
+        counter = 0.0
+        timeLabel.text = String(counter)
+    }
+    
+    func updateCounter() {
+        counter += 0.1;
+        timeLabel.text = String(format: "%0.1f",counter)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timeLabel.text = String(counter);
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
